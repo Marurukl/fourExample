95b96c9fc4ba1204a5066ae2cf76248567f4d76a
Закарьянов Арнур <>
2017-09-05 17:27:20 +0600

    first

---
+++ ConsoleApp23/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Random rnd = new Random();
+                int[] arr = new int[10];
+                for (int i = 0; i < 11 ; i++)
+                {
+                    arr[i] = rnd.Next(1, 10);
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                Console.WriteLine("Завершение работы массива");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ConsoleApp23/ConsoleApp23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                  WebClient webClient = new WebClient();
+                  webClient.OpenRead(@"http://www.professoweb.ru/");
+
+            }
+            catch (System.Net.WebException)
+            {
+                Console.WriteLine("Введено не правльная ссылка !");
+            }
+            Console.ReadLine();
+        }
+    }
+}
